552ec2b78c73a57aa7fb13b11341fdd782dd66d4
Chad Cuddigan <chad.cuddigan@ticketfly.com>
2015-05-13 16:55:32 -0700

    Fixing up consumer example confusion.

--- examples/consumer.rb
+++ examples/consumer.rb
@@ -1,12 +1,12 @@
 $:.unshift File.expand_path(File.dirname(__FILE__) + '/../lib')
 require 'poseidon'
 
-producer = Poseidon::PartitionConsumer.new("example_consumer", "localhost", 9092,
+consumer = Poseidon::PartitionConsumer.new("example_consumer", "localhost", 9092,
                                            "example", 0, :earliest_offset)
 
 loop do
   begin
-    messages = producer.fetch
+    messages = consumer.fetch
     messages.each do |m|
       puts "Received message: #{m.value}"
     end
